e738e554f53abc1999674c3f7d53259ff9a55a1f
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-14 11:12:25 +0530

    learn advance routing

--- backend/01-express-js/index.js
+++ backend/01-express-js/index.js
@@ -1,11 +1,29 @@
 import express from "express";
 
-// instance of express
+// instance of express...
 const app = express();
 
-// api endpoints (routing)
+// callbacks...
+const callback1 = (req, res, next) => {
+	console.log("Callback 1");
+	next();
+};
 
-// get method
+const callback2 = (req, res, next) => {
+	console.log("Callback 2");
+	next();
+};
+
+const callback3 = (req, res) => {
+	console.log("Callback 3");
+	res.send("<h1>Array of callbacks<h1/>");
+};
+
+// clear terminal...
+console.clear();
+
+// api endpoints (routing)...
+// 1. get method
 app.get("/", (req, res) => {
 	res.send("<h1>Welcome to the home page!<h1/>");
 });
@@ -18,7 +36,29 @@ app.get("/contact", (req, res) => {
 	res.send("<h1>Welcome to the contact page!<h1/>");
 });
 
-// listen to the port
+// REGEX...
+app.get("/x/", (req, res) => {
+	res.send("<h1>If the path includes x, this route will run.<h1/>");
+});
+
+// multiple callbacks ...
+app.get(
+	"/callback-craziness",
+	(req, res, next) => {
+		console.log("First callback!");
+		// res.send("<h1>First callback!<h1/>");
+		next();
+	},
+	(req, res) => {
+		console.log("Second callback!");
+		res.send("<h1>Second callback!<h1/>");
+	}
+);
+
+// array of callbacks...
+app.get("/callback-arrays", [callback1, callback2, callback3]);
+
+// listen to the port...
 app.listen("5000", () => {
 	console.log("Server started on PORT: 5000");
 });
