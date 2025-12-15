1b3ac9533e80f99933119ab817f40e81d7e189da
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-15 22:57:17 +0530

    learn route parametes

--- backend/01-express-js/index.js
+++ backend/01-express-js/index.js
@@ -5,8 +5,20 @@ import managers from "./routes/managers.js";
 // instance of express...
 const app = express();
 
+// using routes
 app.use("/developers", developers);
 app.use("/managers", managers);
+
+// ------------------------------------------------------
+
+// route parameters...
+app.get("/product/:category/:id", (req, res) => {
+	const { category, id } = req.params;
+	console.log(category, id);
+	res.send(`Product Category: ${category} & Product ID: ${id}`);
+});
+
+// ------------------------------------------------------
 
 // listen to the port...
 app.listen("5000", () => {
