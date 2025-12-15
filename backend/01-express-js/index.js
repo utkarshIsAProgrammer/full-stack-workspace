7144acbcb9843cc2649fa7596690284153e27ada
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-15 23:22:25 +0530

    learn query strings

--- backend/01-express-js/index.js
+++ backend/01-express-js/index.js
@@ -11,6 +11,12 @@ app.use("/developers", developers);
 app.use("/managers", managers);
 app.use("/students", students);
 
+// query string...
+app.get("/product", (req, res) => {
+	const { category, id } = req.query;
+	res.send(`Product Category: ${category} & Product ID: ${id}`);
+});
+
 // listen to the port...
 app.listen("5000", () => {
 	console.log("Server started on PORT: 5000");
