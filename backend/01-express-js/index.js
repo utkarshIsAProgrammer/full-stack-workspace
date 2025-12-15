c3f545bdfc77e2ee94f0bc0308457f0a17b9322c
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-15 22:59:31 +0530

    update folder structure

--- backend/01-express-js/index.js
+++ backend/01-express-js/index.js
@@ -2,23 +2,12 @@ import express from "express";
 import developers from "./routes/developers.js";
 import managers from "./routes/managers.js";
 
-// instance of express...
+// express instance...
 const app = express();
 
-// using routes
+// using routes...
 app.use("/developers", developers);
 app.use("/managers", managers);
-
-// ------------------------------------------------------
-
-// route parameters...
-app.get("/product/:category/:id", (req, res) => {
-	const { category, id } = req.params;
-	console.log(category, id);
-	res.send(`Product Category: ${category} & Product ID: ${id}`);
-});
-
-// ------------------------------------------------------
 
 // listen to the port...
 app.listen("5000", () => {
