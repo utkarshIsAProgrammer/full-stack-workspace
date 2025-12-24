b73a9a6f3e0130d1b1a9cf2fa6c38336938d8a75
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-25 03:39:59 +0530

    learn caeat and insert documents to db

--- backend/02-mongodb-mongoose/index.js
+++ backend/02-mongodb-mongoose/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
+import { insertManyDocs } from "./models/Movies.js";
 
 const app = express(); // initialize express
 const port = process.env.PORT || 5500; // env server port
 const DATABASE_URL = process.env.DATABASE_URL; // env mongoose url
 
 connectDB(DATABASE_URL); // connect db
+
+insertManyDocs(); // creating the movie document
 
 // listening to the port
 app.listen(port, () => {
--- backend/02-mongodb-mongoose/models/Movies.js
+++ backend/02-mongodb-mongoose/models/Movies.js
@@ -3,13 +3,13 @@ import mongoose, { mongo } from "mongoose";
 // define schema
 const movieSchema = new mongoose.Schema({
 	name: {
-		type: string,
+		type: String,
 		required: true,
 		trim: true,
 	},
 
 	ratings: {
-		type: number,
+		type: Number,
 		required: true,
 		trim: true,
 		min: 1,
@@ -36,3 +36,54 @@ const movieSchema = new mongoose.Schema({
 		},
 	],
 });
+
+// creating model
+const MovieModel = mongoose.model("Movie", movieSchema); // "Movie" is converted into "movies" when executed
+
+// creating document
+const insertManyDocs = async () => {
+	try {
+		const m1 = new MovieModel({
+			name: "Extraction",
+			ratings: 4.5,
+			money: 600000,
+			genre: ["action", "adventure"],
+			isActive: true,
+			comments: [{ value: "Amazing..." }],
+		});
+
+		const m2 = new MovieModel({
+			name: "Endgame",
+			ratings: 5,
+			money: 1000000,
+			genre: ["action", "marvel", "sci-fi"],
+			isActive: true,
+			comments: [{ value: "Fantastic marvel movie..." }],
+		});
+
+		const m3 = new MovieModel({
+			name: "IT",
+			ratings: 4.2,
+			money: 50000,
+			genre: ["horror", "adventure", "mystery"],
+			isActive: true,
+			comments: [{ value: "Best entertaining mystery series..." }],
+		});
+
+		const m4 = new MovieModel({
+			name: "From",
+			ratings: 4.8,
+			money: 430000,
+			genre: ["mystery", "horror", "adventure", "thriller", "adventure"],
+			isActive: true,
+			comments: [{ value: "Great story line..." }],
+		});
+
+		const result = await MovieModel.insertMany([m1, m2, m3, m4]);
+		// console.log(result);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export { insertManyDocs };
