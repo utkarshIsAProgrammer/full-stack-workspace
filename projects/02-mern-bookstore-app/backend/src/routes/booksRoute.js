74d17aad120e8c9a7e998e4f65e889a6b2069f32
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-01-01 00:29:21 +0530

    build mern bookstore backend

---
+++ projects/02-mern-bookstore-app/backend/src/routes/booksRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+	getAllBooks,
+	getBookById,
+	addBook,
+	updateBook,
+	deleteBook,
+} from "../controllers/booksController.js";
+
+const router = express.Router();
+
+router.get("/get-all-books", getAllBooks);
+router.get("/get-book-by-id/:id", getBookById);
+router.post("/add-book", addBook);
+router.put("/update-book/:id", updateBook);
+router.delete("/delete-book/:id", deleteBook);
+
+export default router;
