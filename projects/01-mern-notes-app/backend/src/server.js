91c3dc829a1a5c1f9958df09e620a235a031d331
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-12-31 19:12:14 +0530

    complete notes app backend

--- projects/01-mern-notes-app/backend/src/server.js
+++ projects/01-mern-notes-app/backend/src/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import "dotenv/config";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 const port = process.env.PORT || 5000; // .env --> port
 
 const app = express();
 
-connectDB(); // connect database
-
-app.use(express.json());
+app.use(express.json()); // middleware (this on allows access to the json request body)
+app.use(rateLimiter); // middleware to ratelimit
 app.use("/api/notes", notesRoutes); // using route
 
-app.listen(port, () => {
-	console.log(`Server running on PORT: ${port} `);
+// connect databse first then listen to the port
+connectDB().then(() => {
+	app.listen(port, () => {
+		console.log(`Server running on PORT: ${port}`);
+	});
 });
