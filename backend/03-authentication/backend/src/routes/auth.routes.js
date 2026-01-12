3d022f63da670f9743dcbcdd9281dba4a3224ef5
IndieDev <utkarshprogrammer2005@gmail.com>
2026-01-13 01:09:20 +0530

    configure directory

--- backend/03-authentication/backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, signin, logout } from "../controllers/auth.controllers.js";
-
-export const router = express.Router();
-
-router.get("/signup", signup);
-router.get("/signin", signin);
-router.get("/logout", logout);
--- frontend/04-react/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-	return (
-		<footer>
-			<p>&copy; 2026 My Website</p>
-		</footer>
-	);
-};
