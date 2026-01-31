125aac971015d21c2881fc901d43f60c05db4082
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-01-31 14:29:46 +0530

    create backend

--- mern-playground/product-store/backend/server.js
+++ mern-playground/product-store/backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+import cors from "cors";
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.get("/home", (req, res) => {
-	res.send("Welcome to the homepage!");
-});
+app.use(cors);
+app.use(express.json());
+app.use("/api/products", productRoutes);
 
 connectDB().then(() => {
 	app.listen(port, () => {
