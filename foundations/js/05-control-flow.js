94dc3a558323e91e53bc9cbb5cdcc72eb31f642f
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-02-06 21:23:17 +0530

    learn control flow statements

---
+++ foundations/js/05-control-flow.js
@@ -0,0 +1,37 @@
+// ---------------------------------------------------------------------------------------
+let money = 780;
+
+if (money < 5000) {
+	console.log("You need $5000 for shopping!");
+} else if (money) {
+	console.log("Go and buy some groceries from the store!");
+} else {
+	console.log("Get some money and go to the supermarket!");
+}
+// ---------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------------
+let age = 18;
+
+if (age <= 0) {
+	console.log("Hey! be serious in your life and never joke like this again.");
+} else if (age >= 120) {
+	console.log(
+		"I'll call the priest! there is no space for ghosts to watch this kind of content.",
+	);
+} else if (age === 18) {
+	console.log(
+		"You are 18, but it doesn't mean that you are mature enough to watch this content. You have to submit an application to our staff then maybe you can get a chance to watch this content.",
+	);
+} else if (age < 18) {
+	console.log(
+		"Hello baby! why are you here you should be in a kinder garden.",
+	);
+} else if (age > 60) {
+	console.log(
+		"Hey Mr! don't you think you are too old to watch this kind of stuff.",
+	);
+} else {
+	console.log("Welcome sir! feel free to have a seat and enjoy the show.");
+}
+// ---------------------------------------------------------------------------------------
