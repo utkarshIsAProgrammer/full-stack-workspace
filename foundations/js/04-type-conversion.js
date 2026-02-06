955e86ed3cf1222350155f686f572005f9337ce6
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-02-06 21:03:06 +0530

    learn type conversion

---
+++ foundations/js/04-type-conversion.js
@@ -0,0 +1,16 @@
+// ---------------------------------------------------------------
+// string to number
+let money = "50";
+console.log(typeof parseInt(money));
+console.log(typeof Number(money));
+
+// number to string
+money = 50;
+console.log(typeof toString(money));
+console.log(typeof String(money));
+
+// string to decimal
+money = "50.09";
+console.log(parseFloat(money));
+console.log(typeof parseFloat(money));
+// ---------------------------------------------------------------
