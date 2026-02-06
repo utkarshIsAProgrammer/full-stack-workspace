f886e41f4304c7440cb2d3539a035cf1e19329b0
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-02-06 21:31:23 +0530

    learn switch statement

---
+++ foundations/js/06-switch-statement.js
@@ -0,0 +1,38 @@
+// --------------------------------------------------------------------------------
+let day = "Sunday";
+
+switch (day) {
+	case "Monday":
+		console.log("It's monday, start your week with positive energy!");
+		break;
+
+	case "Tuesday":
+		console.log("It's tuesday, keep grinding!");
+		break;
+
+	case "Wednesday":
+		console.log("It's wednesday, keep grinding and review you work!");
+		break;
+
+	case "Thursday":
+		console.log("It's thursday, finish up you pending tasks!");
+		break;
+
+	case "Friday":
+		console.log("It's friday, review your work for the entire week!");
+		break;
+
+	case "Saturday":
+		console.log("It's saturday, relax and chill. Enjoy your holiday!");
+		break;
+
+	case "Sunday":
+		console.log(
+			"It's sunday, setup goals for the next week and get back into working mode!",
+		);
+		break;
+
+	default:
+		console.log("You may have some special plans for this day!");
+}
+// --------------------------------------------------------------------------------
