c6cf4d4f7cdb2f654da54b6f4d040d93a53d6026
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-02-15 16:35:49 +0530

    start reactJs

---
+++ frontend/reactJs/src/App.jsx
@@ -0,0 +1,10 @@
+import { Greet } from "./components/Greet";
+
+export const App = () => {
+	return (
+		<div>
+			<h1>Welcome to the react tutorial!</h1>
+			<Greet />
+		</div>
+	);
+};
---
+++ frontend/reactJs/src/components/Greet.jsx
@@ -0,0 +1,3 @@
+export const Greet = () => {
+	return <h1>Welcome to the JSX greeting ceremony!</h1>;
+};
