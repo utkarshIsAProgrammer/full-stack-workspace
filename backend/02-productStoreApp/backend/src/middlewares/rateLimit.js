42dd1f3f48232ea4e260701fcf11fa3f95406b84
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-02-18 11:47:22 +0530

    correct ratelimit time

--- backend/02-productStoreApp/backend/src/middlewares/rateLimit.js
+++ backend/02-productStoreApp/backend/src/middlewares/rateLimit.js
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minute
-	max: 2, // limit each IP to 100 requests
+	max: 100, // limit each IP to 100 requests
 	message: "Too many requests, try again later!",
 });
