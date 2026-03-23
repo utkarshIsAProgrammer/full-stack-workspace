07157c6669c810f63fb46a8c84f3ee9da725849b
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-03-23 12:18:46 +0530

    add user auth

---
+++ backend/09-expenseTracker/backend/src/controllers/auth.controllers.js
@@ -0,0 +1,121 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+	const { name, email, password } = req.body;
+
+	try {
+		if (!name || !email || !password) {
+			return res.status(400).json({
+				success: false,
+				message: "All fields must be provided!",
+			});
+		}
+
+		const emailRegex =
+			/^(?!.*\.\.)(?!.*\.$)[^\W][A-Za-z0-9._%+-]{0,63}@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,}$/;
+		if (!emailRegex.test(email)) {
+			return res
+				.status(400)
+				.json({ success: false, message: "Invalid email format!" });
+		}
+
+		if (password.length < 6) {
+			return res.status(400).json({
+				success: false,
+				message: "Password must be at least 6 characters long!",
+			});
+		}
+
+		const userExists = await User.findOne({ email });
+		if (userExists) {
+			return res
+				.status(400)
+				.json({ success: false, message: "User already exists!" });
+		}
+
+		const user = await User.create({ name, email, password });
+
+		const token = await user.signToken({ userId: user._id });
+
+		res.cookie("jwt", token, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: process.env.NODE_ENV === "production",
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+		});
+
+		res.status(201).json({
+			success: true,
+			message: "User created successfully!",
+			user: {
+				userId: user._id,
+				name: user.name,
+				email: user.email,
+			},
+		});
+	} catch (err) {
+		console.log(`Error in the signup controller! ${err.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error!",
+		});
+	}
+};
+
+export const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		if (!email || !password) {
+			return res.status(400).json({
+				success: false,
+				message: "All fields must be provided!",
+			});
+		}
+
+		const existingUser = await User.findOne({ email });
+		if (!existingUser) {
+			return res
+				.status(404)
+				.json({ success: false, message: "User doesn't exist!" });
+		}
+
+		if (!(await existingUser.comparePassword(password))) {
+			return res
+				.status(400)
+				.json({ success: false, message: "Invalid credentials!" });
+		}
+
+		const token = existingUser.signToken({ userId: existingUser._id });
+
+		res.cookie("jwt", token, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: process.env.NODE_ENV === "production",
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+		});
+
+		res.status(200).json({
+			success: true,
+			message: "Logged in successfully!",
+		});
+	} catch (err) {
+		console.log(`Error in the login controller! ${err.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error!",
+		});
+	}
+};
+
+export const logout = async (req, res) => {
+	try {
+		await res.clearCookie("jwt");
+		res.status(200).json({
+			success: true,
+			message: "Logged out successfully!",
+		});
+	} catch (err) {
+		console.log(`Error in the logout controller! ${err.message}`);
+	}
+};
---
+++ backend/09-expenseTracker/backend/src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Name is required!"],
+			trim: true,
+		},
+
+		email: {
+			type: String,
+			required: [true, "Email is required!"],
+			unique: true,
+			trim: true,
+			lowercase: true,
+		},
+
+		password: {
+			type: String,
+			required: [true, "Password is required!"],
+			minLength: [6, "Password must be at least 6 characters long!"],
+		},
+	},
+	{ timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+	if (!this.isModified("password")) {
+		return;
+	}
+
+	try {
+		this.password = await bcrypt.hash(this.password, 10);
+	} catch (err) {
+		console.log(`Error hashing password!`);
+	}
+});
+
+userSchema.methods.signToken = async function (userId) {
+	jwt.sign(userId, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+userSchema.methods.comparePassword = async function (password) {
+	try {
+		return await bcrypt.compare(password, this.password);
+	} catch (err) {
+		console.log(`Error comparing password! ${err.message}`);
+	}
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
