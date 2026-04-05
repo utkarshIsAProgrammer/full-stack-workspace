dfbbfa179695be3bdea5bfec669e3cfe1d9fc7bf
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-04-05 14:01:58 +0530

    learn literals

--- foundations/ts/index.ts
+++ foundations/ts/index.ts
@@ -74,3 +74,152 @@ const half = (a: number): number => {
 const halfRes = half(5);
 console.log(halfRes);
 // -------------------------------------------------------------------------------------------------------------------------
+
+// * VOID
+// -------------------------------------------------------------------------------------------------------------------------
+function sayHello(): void {
+	console.log("Hello, Geek!");
+}
+sayHello();
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * NEVER
+// -------------------------------------------------------------------------------------------------------------------------
+function throwErr(msg: string): never {
+	throw new Error(msg);
+}
+// throwErr("Throw Error!");
+
+let nope: never;
+function neverReturns(): never {
+	while (true) {}
+}
+// nope = neverReturns();
+
+function infiniteLoop(): never {
+	while (true) {
+		console.log("NEVER, Infinite Loop!");
+	}
+}
+// infiniteLoop();
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * ARRAY
+// -------------------------------------------------------------------------------------------------------------------------
+let numArr: number[] = [1, 2, 3]; // []notation syntax
+console.log(numArr);
+
+let strArr: Array<string> = ["a", "b", "c"]; // Array<type> syntax
+console.log(strArr);
+strArr.push("d");
+strArr.push("e");
+strArr.push("f");
+console.log(strArr);
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * MULTIDIMENSIONAL ARRAYS
+// -------------------------------------------------------------------------------------------------------------------------
+let mdArr: number[][] = [[1, 2, 3]];
+console.log(mdArr);
+
+let tripleMdArr: number[][][] = [[[1, 2, 3]]];
+console.log(tripleMdArr);
+// -------------------------------------------------------------------------------------------------------------------------
+
+//  * OBJECTS
+// -------------------------------------------------------------------------------------------------------------------------
+const employee: {
+	name: string;
+	email: string;
+	role: string;
+	age: number;
+	id: number;
+} = {
+	name: "IndieDev",
+	email: "indieDev@gmail.com",
+	role: "Fullstack Software Developer",
+	age: 20,
+	id: 589385293412,
+};
+console.log(employee);
+console.log(employee.name, "|", employee.role);
+
+function printUser(): { name: string; age: number; location: string } {
+	return { name: "IndieDev", age: 20, location: "Germany" };
+}
+const res = printUser();
+console.log(res);
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * TYPE ALIASES
+// -------------------------------------------------------------------------------------------------------------------------
+type Person = {
+	name: string;
+	age: number;
+};
+
+function printPerson(person: Person) {
+	console.log(person.name, "|", person.age);
+}
+printPerson({
+	name: "Devlooper",
+	age: 20,
+});
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * OPTIONAL PROPERTIES AND READONLY PROPERTY
+// -------------------------------------------------------------------------------------------------------------------------
+type someDev = {
+	name: string;
+	age?: number;
+	readonly role: string;
+};
+
+function printDev(dev: someDev) {
+	console.log(dev.name, "|", dev.age, "|", dev.role);
+}
+printDev({ name: "Devlooper", role: "SDE" });
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * INTERSECTION TYPE
+// -------------------------------------------------------------------------------------------------------------------------
+type man = {
+	name: string;
+	age?: number;
+};
+
+type employee = {
+	readonly id: number;
+	role: string;
+};
+
+type manAndEmployee = man & employee;
+
+const geek: manAndEmployee = {
+	name: "Devlooper",
+	id: 113129312882,
+	role: "SDE",
+};
+console.log(geek);
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * UNIONS
+// -------------------------------------------------------------------------------------------------------------------------
+let unionVar: number | string = 20;
+console.log(unionVar);
+
+const items: (number | string)[] = [1, 2, 3, "one", "two", "three"];
+console.log(items);
+// -------------------------------------------------------------------------------------------------------------------------
+
+// * LITERALS
+// -------------------------------------------------------------------------------------------------------------------------
+let aA: 1 | 2;
+aA = 1;
+aA = 2;
+// aA = 3; // !ERROR
+console.log(aA);
+// -------------------------------------------------------------------------------------------------------------------------
+
+// TODO: CONTINUE LEARNING FROM TUPLES
