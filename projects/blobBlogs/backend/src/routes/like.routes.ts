845f4ab4012f84339bfa67286c5cd0045b131896
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-05-14 01:23:01 +0530

    remove a dir/ and fix blobBlogs bugs

--- projects/blobBlogs/backend/src/routes/like.routes.ts
+++ projects/blobBlogs/backend/src/routes/like.routes.ts
@@ -1,18 +1,13 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware";
 import {
-	togglePostLikes,
-	toggleCommentLikes,
-	// getPostLikes,
-	// getCommentLikes,
+  togglePostLikes,
+  toggleCommentLikes,
 } from "../controllers/like.controllers";
 
 const router = express.Router();
 
 router.post("/post/:postId", protect, togglePostLikes);
-// router.get("/post/:postId", protect, getPostLikes);
-
 router.post("/comment/:commentId", protect, toggleCommentLikes);
-// router.get("/comment/:commentId", protect, getCommentLikes);
 
 export { router as likeRoutes };
--- projects/miniUrls/backend/dist/routes/url.routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as urlRoutes };
-//# sourceMappingURL=url.routes.d.ts.map
