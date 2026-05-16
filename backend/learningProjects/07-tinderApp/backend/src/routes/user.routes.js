d00fe8b356146cfeda491c85e779dfae5a660bd3
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-05-16 10:09:46 +0530

    fix repo

--- backend/learningProjects/07-tinderApp/backend/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protectRoute } from "../middlewares/auth.middleware.js";
-import { updateProfile } from "../controllers/user.controllers.js";
-
-const router = express.Router();
-
-router.put("/update", protectRoute, updateProfile);
-
-export { router as userRoutes };
--- backend/learningProjects/08-simpleChatApp/backend/src/utils/setCookie.js
+++
@@ -1,8 +0,0 @@
-export const setCookie = (res, token) => {
-	res.cookie("jwt", token, {
-		maxAge: 7 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
-		sameSite: "strict",
-		secure: process.env.NODE_ENV !== "development",
-	});
-};
